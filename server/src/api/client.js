a09a8eec78f5131037bc847c69408113b4f628ff
Santiago-PM <spm06.ab2023@gmail.com>
2026-04-28 18:22:34 +0200

    Finalización de Actividad 3

--- server/src/api/client.js
+++ server/src/api/client.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/api/v1/tasks';
+const API_URL = 'https://taskflow-project-q6po.vercel.app/api/v1/tasks';
 
 export async function cargarTareas() {
   const res = await fetch(API_URL);
